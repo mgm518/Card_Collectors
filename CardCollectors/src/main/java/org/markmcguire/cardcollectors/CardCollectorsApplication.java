fc708f6aa405301e915adb2bfa74bbc0c426e046
mgm518 <mgm518@yahoo.com>
2024-06-07 14:25:33 -0700

    Created base project

---
+++ CardCollectors/src/main/java/org/markmcguire/cardcollectors/CardCollectorsApplication.java
@@ -0,0 +1,13 @@
+package org.markmcguire.cardcollectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCollectorsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CardCollectorsApplication.class, args);
+  }
+
+}
